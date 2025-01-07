346666d29a6ff7e21c2ff40643d35b917e8d050e
Its-Am1r <itsam1r@gmail.com>
2025-01-07 14:38:44 +0330

    init front

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthService } from './Services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front';
+    // User Status
+    isLoggedIn!: boolean;
+    constructor(private authService : AuthService){
+
+    }
+
+    ngOnInit(): void {
+      this.isLoggedIn = !!this.authService.getToken();
+    }
+
+  //Method to
+  getUserName() {
+    return this.authService.getUser();
+  }
+    //Method to logout
+    signOut() {
+      this.authService.signOut();
+    }
 }
